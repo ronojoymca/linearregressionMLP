90de93b4a91c4c5d175b6360acbc0ff6a718e662
Ranajoy Chatterjee <38803790+ronojoymca@users.noreply.github.com>
2018-04-28 11:54:41 +0530

    Add files via upload

---
+++ MLTest.java
@@ -0,0 +1,15 @@
+package ekistics.nukkadkart.utilities;
+
+public class MLTest {
+	
+	public static void main(String args[]) {
+		LinearRegressionDemo lrd=new LinearRegressionDemo();
+		try {
+			lrd.process();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
